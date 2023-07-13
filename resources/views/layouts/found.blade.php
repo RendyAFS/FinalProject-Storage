fc27ace88316bffb8a728bbaa345596900545848
RendyAFS <arendy724@gmail.com>
2023-07-13 10:40:45 +0700

    update Create Found dan Fitur menampilkan foto

--- resources/views/layouts/found.blade.php
+++ resources/views/layouts/found.blade.php
@@ -17,22 +17,30 @@
     <table class="table table-bordered table-hover table-striped mb-0 table-dark"
     id="tableFound">
         <thead>
-            <tr class="fs-5">
-                <th>Nama</th>
-                <th>Nama Barang</th>
+            <tr class="fs-5 text-center">
+                <th>Deskripsi Singkat</th>
+                {{-- <th>Nama Barang</th> --}}
                 <th>Tanggal ditemukan</th>
                 <th>Tanggal Claim</th>
-                <th>Action</th>
+                {{-- <th>Action</th> --}}
             </tr>
         </thead>
         <tbody>
             @foreach ($founds as $found)
-                <tr>
-                    <td>{{ $found->nama }}</td>
-                    <td>{{ $found->nama_barang }}</td>
-                    <td>{{ $found->tgl_ditemukan }}</td>
-                    <td>{{ $found->tgl_claim }}</td>
-                    <td>@include('action.actionfound')</td>
+                <tr class="text-center">
+                    <td>
+                        {{ $found->nama_barang }} <br><br>
+                        {{-- {{ $found->foto_barang_found }} --}}
+                        <img src="{{ asset('foto-found/'.$found->foto_barang_found)}}" style="width: 150px">
+                        <br><br>
+                        <div>
+                            @include('action.actionfound')
+                        </div>
+                    </td>
+                    {{-- <td> </td> --}}
+                    <td class="align-middle">{{ $found->tgl_ditemukan }}</td>
+                    <td>{{ $found->deleted_at }}</td>
+                    {{-- <td></td> --}}
                 </tr>
             @endforeach
         </tbody>
