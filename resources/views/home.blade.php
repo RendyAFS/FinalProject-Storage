537fb200f0651bcb3c8ccf7bda29543d1b074780
RendyAFS <arendy724@gmail.com>
2023-07-23 14:40:09 +0700

    Fitur Server side Processing

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <div class="container">
-        <div class="row" id="logo-home">
+        <div class="row pt-5" id="logo-home">
             <div class="col" style="color: white">
                 <img src="{{ asset('/images/lo-fo hori.png')}}" style="width: 350px">
             </div>
