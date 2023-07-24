4960a7305b9c66993fab5d240b5d5433874ebb76
RendyAFS <arendy724@gmail.com>
2023-07-24 12:56:39 +0700

    Minor Icon

--- resources/views/layouts/appdisplay.blade.php
+++ resources/views/layouts/appdisplay.blade.php
@@ -26,7 +26,7 @@
         <nav class="navbar navbar-expand-md navbar-light shadow-sm" id="navbar">
             <div class="container">
                 <a class="navbar-brand fw-bold" href="{{ route('founds.index') }}">
-                    <img src="{{ asset('/images/Asset 1.png')}}" style="width: 25px"> LOST & FOUND
+                    <i class="bi bi-house-fill"></i> LOST & FOUND
                 </a>
                 <button class="navbar-toggler"  type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon" style="color: #FFA559;"  ></span>
